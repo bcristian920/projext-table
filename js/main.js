11e862c4d7516897e170786fe52caae7e8506fc2
Miguel Eduardo Newan <men18248597@gmail.com>
2024-06-05 19:22:52 -0300

    initial project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*const brands = ["Nike", "Adidas", "Apple", "Google", "Microsoft", "Pepsi", "Secco","Toyota"];
+let arrFinal = [];
+brands.forEach((brand) => {
+    if (brand==="Google")
+        console.log(brand);
+    else
+        arrFinal.push(brand);
+});
+console.log(arrFinal);*/
